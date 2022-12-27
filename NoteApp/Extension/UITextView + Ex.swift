3b1cf7c59cf71aca8779f10152788e52e22ad93c
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2022-12-28 01:33:45 +0300

    Updated UITextField and UITextView extensions

--- NoteApp/Extension/UITextView + Ex.swift
+++ NoteApp/Extension/UITextView + Ex.swift
@@ -1,22 +1,19 @@
 //
-//  UITextField + Ex.swift
+//  UITextView + Ex.swift
 //  NoteApp
 //
-//  Created by Ivan Puzanov on 23.12.2022.
+//  Created by Ivan Puzanov on 28.12.2022.
 //
 
 import UIKit
 
-extension UITextField {
-    func configureWith(fontSize: CGFloat, fontWeight: UIFont.Weight) {
-        self.font = UIFont.systemFont(ofSize: fontSize, weight: fontWeight)
-    }
-    
+extension UITextView {
     func configureToolbar() {
         let doneToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
         doneToolbar.barStyle = .default
         
         let flexSpace   = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
         let done        = UIBarButtonItem(title: NSLocalizedString("Done", comment: ""), style: .done, target: self, action: #selector(resignFirstResponder))
         done.tintColor  = Color.appAccentColor
         
